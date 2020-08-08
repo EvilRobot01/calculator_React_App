13306a315eed260ab8c04b426ca14924e738ae89
HaroldDRF <95robots@gmail.com>
2020-08-08 12:18:58 -0700

    epense item compoent display exppenses

--- calculator/src/components/ExpenseItem.js
+++ calculator/src/components/ExpenseItem.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import {MdEdit, MdDelete} from 'react-icons/md';
 
-const ExpenseItem = () => {
+const ExpenseItem = ({expense}) => {
+    const {id, charge, amount} = expense;
     return (
-        <div>
-                ExpenseItem
+        <li className='item'>
+        <div className="info">
+            <span className='expense'>{charge}</span>
+            <span className='amount'>{amount}</span>
         </div>
-    )
-}
+        <div className='edit-btn' aria-label='edit button'>
+            <MdEdit />
+        </div>
+        <div className='clear-btn' aria-label='delete button'>
+            <MdDelete />
+        </div>
+
+        </li>
+    );
+};
 
 export default ExpenseItem;
